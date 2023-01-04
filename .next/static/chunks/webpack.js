850cdf037e947d2108015de1f80b22982133fa4a
Reiju_0 <riddwan.a@gmail.com>
2023-01-04 13:22:03 +0700

    Push next

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "c7c165ec1d639f01"; }
+/******/ 		__webpack_require__.h = function() { return "86b52eeb91ebf27f"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
